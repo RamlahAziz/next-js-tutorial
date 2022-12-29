47b734cf257a30fd03441af8c9cff2d694adff8a
Ramlah Aziz <ramlah.aziz2012@gmail.com>
2022-12-29 15:18:50 +0500

    dynamic routing

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,44 +1,53 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    return {
-        props: {
-            allPostsData,
-        },
-    };
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-        <Head>
-            <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-            <p>Hi, I am a software engineer learning Next.js! I love to read and binge watch fantasy shows. If you want to chat or send me any recommendations, you can contact me on <a href="https://www.linkedin.com/in/ramlahaziz/">LinkedIn</a>!</p>
-            <p>
-                (This is a sample website from the official Next.js start tutorial. Click <a href="https://nextjs.org/learn">here</a> to build a site just like this one.)
-            </p>
-        </section>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi, I am a software engineer learning Next.js! I love to read and
+          binge watch fantasy shows. If you want to chat or send me any
+          recommendations, you can contact me on{" "}
+          <a href="https://www.linkedin.com/in/ramlahaziz/">LinkedIn</a>!
+        </p>
+        <p>
+          (This is a sample website from the official Next.js start tutorial.
+          Click <a href="https://nextjs.org/learn">here</a> to build a site just
+          like this one.)
+        </p>
+      </section>
 
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
-            <ul className={utilStyles.list}>
-                {allPostsData.map(({ id, date, title }) => (
-                    <li className={utilStyles.listItem} key={id}>
-                        {title}
-                        <br />
-                        {id}
-                        <br />
-                        {date}
-                    </li>
-                ))}
-            </ul>
-        </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  )
+  );
 }
